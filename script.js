a8238b62dcef1f26c85524802cf6377f36ddf7a6
Raymond <raymond.vuong@torontomu.ca>
2026-09-14 14:59:59 -0400

    Set up calculator display

---
+++ script.js
@@ -0,0 +1,28 @@
+const add = (a, b) => {
+    return a + b
+}
+
+const subtract = (a, b) => {
+    return a - b
+}
+
+const multiply = (a, b) => {
+    return a * b
+}
+
+const divide = (a, b) => {
+    return a / b
+}
+
+const operate = (a, b, operator) => {
+    if (operator === '+') {
+        return add(a, b)
+    } else if (operator === '-') {
+        return subtract(a, b)
+    } else if (operator === '*') {
+        return multiply(a, b)
+    } else if (operator === '/') {
+        return divide(a, b)
+    }
+}
+
