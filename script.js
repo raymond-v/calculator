9b4b8605f4cadfced4d018fce7c5a6865891d0d2
Raymond <raymond.vuong@torontomu.ca>
2026-09-14 18:10:51 -0400

    Implement calculator functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const add = (a, b) => {
-    return a + b
+    return Number(a) + Number(b)
 }
 
 const subtract = (a, b) => {
@@ -26,3 +26,58 @@ const operate = (a, b, operator) => {
     }
 }
 
+const buttons = document.querySelectorAll('button')
+const display = document.querySelector('.display')
+let a = ''
+let b = ''
+let operator = ''
+
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        if (event.target.classList.contains('number')) {
+            if (b == '' && operator == '') {
+                a += event.target.value
+            } else if (a && operator) {
+                b += event.target.value
+            }
+            if (display.textContent == '0') {
+                display.textContent = ''
+                display.textContent += event.target.value
+            } else {
+                display.textContent += event.target.value
+            }
+        } else if (event.target.classList.contains('dot')) {
+            if (b == '' && operator == '' && !a.includes('.')) {
+                a += event.target.value
+                display.textContent += event.target.value
+            } else if (a && operator && !b.includes('.')) {
+                b += event.target.value
+                display.textContent += event.target.value
+            }
+        } else if (event.target.classList.contains('sign')) {
+            if (a && b == '') {
+                operator = event.target.value
+            } else if (a && b) {
+                const results = operate(a, b, operator)
+                a = results
+                operator = event.target.value
+                b = ''
+                display.textContent = a
+            }
+            const signs = /[+\-*\/]/
+            if (signs.test(display.textContent)) {
+                display.textContent = display.textContent.replace(signs, event.target.value)
+            } else {
+            display.textContent += ` ${event.target.value} `
+            }
+        } else if (event.target.classList.contains('clear')) {
+            a = ''
+            b = ''
+            operator = ''
+            display.textContent = '0'
+        } else if (event.target.classList.contains('equal')) {
+            const results = operate(a, b, operator)
+            display.textContent = results
+        }
+    })
+})
